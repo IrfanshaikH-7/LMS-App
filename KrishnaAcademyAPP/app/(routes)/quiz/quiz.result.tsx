35795c456c599351052743683cc3d22e58292a22
IrfanshaikH-7 <414irfann@gmail.com>
2024-08-06 19:14:21 +0530

    new quiz look

--- KrishnaAcademyAPP/app/(routes)/quiz/quiz.result.tsx
+++ KrishnaAcademyAPP/app/(routes)/quiz/quiz.result.tsx
@@ -1,10 +1,28 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { View, Text, FlatList } from 'react-native';
+import { SafeAreaView } from "react-native-safe-area-context";
 
-
+const arrayOfObjects = [
+  { color: 'red', title: 'Apple', alpha: 'a' },
+  { color: 'blue', title: 'Blueberry', alpha: 'b' },
+  { color: 'green', title: 'Grapes', alpha: 'c' },
+  { color: 'yellow', title: 'Lemon', alpha: 'd' },
+  { color: 'orange', title: 'Orange', alpha: 'e' },
+  { color: 'purple', title: 'Plum', alpha: 'f' }
+];
 export default function quizresult() {
   return (
-    <View>
-      <Text>quiz-result</Text>
-    </View>
+    <SafeAreaView style={{padding:12}}>
+      <FlatList
+      data={arrayOfObjects}
+      numColumns={3}
+      renderItem={({ item }) => (
+        <View style={{ flex: 1,alignItems:'center', justifyContent:'center', padding: 10,margin:8,borderRadius: 14, height:100,borderWidth:1, borderColor: item.color }}>
+        <Text>{item.alpha}</Text>
+        <Text>{item.title}</Text>
+      </View>)}
+      keyExtractor={(item) => item.title}
+    />
+    </SafeAreaView>
   )
 }
